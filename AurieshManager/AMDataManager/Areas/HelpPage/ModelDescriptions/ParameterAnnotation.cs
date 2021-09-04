97b89cbf1c03918739c2eab2786e3fddb385570e
rave-25 <raven.reyes250@gmail.com>
2021-09-04 18:24:50 +0800

    Added Web API with Authenticaions

---
+++ AurieshManager/AMDataManager/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AMDataManager.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
---
+++ AurieshManager/AMDataManager/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(AMDataManager.Startup))]
+
+namespace AMDataManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
